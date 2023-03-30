281e2524ba854303745c4d64b0ca2d0435acc834
maof97 <martin.offermann@outlook.com>
2023-03-30 18:49:25 +0000

    Script created

--- script.py
+++ script.py
@@ -1,28 +1,89 @@
 import os
-from google.oauth2.credentials import Credentials
+import logging
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from matrix_client.api import MatrixHttpApi
+import matrix_client.api
+import matrix_client.errors
 
-# Authenticate with the Gmail API
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s %(message)s",
+    handlers=[
+        logging.FileHandler("script.log"),
+        logging.StreamHandler()
+    ]
+)
+
+# Define constants
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SERVICE_ACCOUNT_FILE = '/path/to/service-account.json'
+MATRIX_USER_ID = '@your_matrix_user_id:your_matrix_server'
+MATRIX_ACCESS_TOKEN = 'your_matrix_access_token'
+MATRIX_ROOM_ID = '!your_room_id:your_matrix_server'
+
+# Create credentials object
 creds = None
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.modify'])
-service = build('gmail', 'v1', credentials=creds)
+try:
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+except FileNotFoundError:
+    logging.error("Service account file not found.")
+except Exception as e:
+    logging.error(f"Unable to create credentials object: {str(e)}")
 
-# Search for unread messages with the "PW Reset Mails" label
-result = service.users().messages().list(userId='me', q='label:PW Reset Mails is:unread').execute()
-messages = result.get('messages', [])
+# Create Gmail API client
+gmail_service = None
+if creds:
+    try:
+        gmail_service = build('gmail', 'v1', credentials=creds)
+    except Exception as e:
+        logging.error(f"Unable to create Gmail API client: {str(e)}")
 
-# Store the sender and subject of each unread message in a string
-message_info = ""
-for message in messages:
-    msg = service.users().messages().get(userId='me', id=message['id']).execute()
-    headers = msg['payload']['headers']
-    sender = [header['value'] for header in headers if header['name'] == 'From'][0]
-    subject = [header['value'] for header in headers if header['name'] == 'Subject'][0]
-    message_info += f"Sender: {sender}\nSubject: {subject}\n\n"
+# Create Matrix API client
+matrix_client = matrix_client.api.MatrixHttpApi(
+    'https://your_matrix_server', token=MATRIX_ACCESS_TOKEN)
 
-# Send the message info to a Matrix channel using a bot user account
-matrix_api = MatrixHttpApi("https://your_matrix_server.com", access_token="your_bot_access_token")
-room_id = "!your_room_id:your_matrix_server.com"
-matrix_api.send_message(room_id, message_info)
+# Check if credentials and API clients are initialized successfully
+if not creds or not gmail_service or not matrix_client:
+    logging.error("Initialization failed. Exiting.")
+    exit()
+
+# Search for unread messages with specific label
+query = "is:unread label:PW Reset Mails"
+try:
+    response = gmail_service.users().messages().list(userId='me', q=query).execute()
+    messages = response.get('messages', [])
+except Exception as e:
+    logging.error(f"Unable to search for messages: {str(e)}")
+    exit()
+
+# If there are unread messages, send an alert to Matrix
+if messages:
+    logging.info(f"{len(messages)} unread messages found.")
+    alert_message = ""
+    for msg in messages:
+        try:
+            message = gmail_service.users().messages().get(userId='me', id=msg['id']).execute()
+            sender = message['payload']['headers'][0]['value']
+            subject = message['payload']['headers'][16]['value']
+            alert_message += f"Sender: {sender}\nSubject: {subject}\n\n"
+        except Exception as e:
+            logging.error(f"Unable to retrieve message: {str(e)}")
+
+    # Send the alert to Matrix
+    try:
+        matrix_client.send_message_event(
+            room_id=MATRIX_ROOM_ID,
+            event_type="m.room.message",
+            content={
+                "msgtype": "m.text",
+                "body": f"Unread messages in Gmail:\n{alert_message}"
+            },
+            user_id=MATRIX_USER_ID
+        )
+        logging.info("Alert sent to Matrix successfully.")
+    except matrix_client.errors.MatrixRequestError as e:
+        logging.error(f"Unable to send alert to Matrix: {str(e)}")
+else:
+    logging.info("No unread messages found.")
